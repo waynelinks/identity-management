4e9ba8f80db8612018f69aeca1043864bd457dc1
Wayne Links <wayne.links@gmail.com>
2020-12-20 03:29:02 +0200

    build(routes): setup routes controller

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,11 +3,14 @@ import express from 'express'
 
 import { commonMiddleware, errorDeligator, requestTraceId } from './middleware'
 import { notFound } from './middleware/notFound'
+import { routes } from './routes/v1'
 
 const app = express()
 
 commonMiddleware(app, express)
 app.use(requestTraceId)
+
+app.use(`${process.env.BASE_API_V1}`, routes)
 
 notFound(app)
 app.use(errorDeligator)
