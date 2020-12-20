9b75bc442dd9d81545b41e897ef979328eff6d46
Wayne Links <wayne.links@gmail.com>
2020-12-21 00:50:09 +0200

    build(user): create signout controller

--- auth/src/controllers/index.ts
+++ auth/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './registerUser'
 export * from './signinUser'
 export * from './currentUser'
+export * from './signoutUser'
--- auth/src/routes/v1/index.ts
+++ auth/src/routes/v1/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 
-import { currentActiveUser, registerUser, signinUser } from '../../controllers'
+import { currentActiveUser, registerUser, signinUser, signoutUser } from '../../controllers'
 import { currentUser, validate } from '../../middleware'
 
 const router = Router()
 
 router.route('/register').post(validate('register'), registerUser)
 router.route('/signin').post(validate('signin'), signinUser)
-
+router.route('/signout').post(validate('signout'), signoutUser)
 router.route('/currentuser').get(currentUser, currentActiveUser)
 
 export { router as routes }
