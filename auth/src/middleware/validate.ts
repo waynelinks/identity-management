566261b3381e7e5b210c15212ac18bba67b4709e
Wayne Links <wayne.links@gmail.com>
2020-12-20 02:35:00 +0200

    build(middleware): create middleware to validate user input upon registration

---
+++ auth/src/middleware/validate.ts
@@ -0,0 +1,34 @@
+import Joi from 'joi'
+import { Request, Response, NextFunction } from 'express'
+
+import { AppError } from '../errors'
+import { CommonErrors, HttpStatusCode } from '../enums'
+
+function validateRegistration(req: Request) {
+  return Joi.object({
+    firstName: Joi.string().required().trim(),
+    lastName: Joi.string().required().trim(),
+    email: Joi.string().required().email().trim(),
+    password: Joi.string().required().min(5),
+    isAdmin: Joi.boolean().optional(),
+  }).validate(req)
+}
+
+export const validate = (validator: string) => (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+): void => {
+  switch (validator) {
+    case 'register': {
+      const { error } = validateRegistration(req.body)
+      if (error) throw new AppError(CommonErrors.BAD_REQUEST, HttpStatusCode.BAD_REQUEST, error.details[0].message, true)
+
+      break
+    }
+
+    default:
+      break
+  }
+  next()
+}
